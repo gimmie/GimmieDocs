d8c4a476b953cc27724deef1204b5f0e1cf594c4
Danny <danny@gimmieworld.com>
2013-12-06 19:04:04 +0800

    adjust docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# markdown-guide documentation build configuration file, created by
+# gimmie-guide documentation build configuration file, created by
 # sphinx-quickstart on Wed Aug 15 10:20:33 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Test Doc'
-copyright = u'2012, Tester'
+project = u'Gimmie Guide'
+copyright = u'2014, Gimmieworld Pte Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'markdown-guidedoc'
+htmlhelp_basename = 'gimmie-guidedoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'markdown-guide.tex', u'markdown-guide Documentation',
-   u'Daniel Greenfeld', 'manual'),
+  ('index', 'gimmie-guide.tex', u'Gimmie Guide',
+   u'Gimmieworld', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'markdown-guide', u'markdown-guide Documentation',
-     [u'Daniel Greenfeld'], 1)
+    ('index', 'gimmie-guide', u'Gimmie Guide',
+     [u'Gimmieworld'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'markdown-guide', u'markdown-guide Documentation',
-   u'Daniel Greenfeld', 'markdown-guide', 'One line description of project.',
+  ('index', 'gimmie-guide', u'Gimmie Guide',
+   u'Gimmieworld', 'gimmie-guide', 'Gimmie Documentation Guide.',
    'Miscellaneous'),
 ]
 
